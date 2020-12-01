1a84f3df29dc44097b01864aa1e2d2aedc5815ab
lighter <zeke.kircheis@gmail.com>
2020-12-01 11:29:04 +0900

    ヘッダーの出し分け

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,11 @@ ActiveRecord::Schema.define(version: 2020_11_30_090426) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name", null: false
-    t.text "profile", null: false
-    t.text "occupation", null: false
-    t.text "position", null: false
+    t.text "last_name", null: false
+    t.text "first_name", null: false
+    t.text "last_name_kana", null: false
+    t.text "first_name_kana", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"

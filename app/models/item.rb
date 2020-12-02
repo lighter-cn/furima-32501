ac768e8d6f47a89050fef162dfcb8ac082960996
lighter <zeke.kircheis@gmail.com>
2020-12-02 15:29:23 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_date
+  belongs_to :prefecture
+  belongs_to :shipping_charge
+
   has_one_attached :image
+
+  validates :category,        numericality: { other_than: 1 }
+  validates :condition,       numericality: { other_than: 1 }
+  validates :delivery_date,   numericality: { other_than: 1 }
+  validates :prefecture,      numericality: { other_than: 1 }
+  validates :shipping_charge, numericality: { other_than: 1 }
 end

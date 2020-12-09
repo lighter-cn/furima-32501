2c067df7c388347706eb396b9306e9a1ac786c74
lighter <zeke.kircheis@gmail.com>
2020-12-09 14:07:42 +0900

    user_idとitem_idのバリデーション追加

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,6 +3,8 @@ class Payment
   attr_accessor :post_number, :prefecture_id, :city, :house_number, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
+    validates :user_id
+    validates :item_id
     validates :post_number, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :city

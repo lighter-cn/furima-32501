a600b12e0bf9ea72a8d487f4a3a33ecf49360f49
lighter <zeke.kircheis@gmail.com>
2020-12-09 17:51:57 +0900

    出品者でも購入済みアイテムの編集削除ボタンが表示されないように修正

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Payment, type: :model do
     before do
       @payment = FactoryBot.build(:payment)
     end
-    context "正常時" do
+    context '正常時' do
       it 'すべての入力項目が問題ない場合購入できる' do
         expect(@payment).to be_valid
       end
@@ -16,7 +16,7 @@ RSpec.describe Payment, type: :model do
       end
     end
 
-    context "エラー発生時" do
+    context 'エラー発生時' do
       it 'user_idが空だと保存できないこと' do
         @payment.user_id = nil
         @payment.valid?
@@ -34,55 +34,55 @@ RSpec.describe Payment, type: :model do
         @payment.valid?
         expect(@payment.errors.full_messages).to include("Token can't be blank")
       end
-  
+
       it '郵便番号が空だと保存できないこと' do
         @payment.post_number = nil
         @payment.valid?
         expect(@payment.errors.full_messages).to include("Post number can't be blank")
       end
-  
+
       it '郵便番号にハイフンが無いと保存できないこと' do
         @payment.post_number = '1234567'
         @payment.valid?
         expect(@payment.errors.full_messages).to include('Post number is invalid. Include hyphen(-)')
       end
-  
+
       it '都道府県が空だと保存できないこと' do
         @payment.prefecture_id = nil
         @payment.valid?
         expect(@payment.errors.full_messages).to include("Prefecture can't be blank")
       end
-  
+
       it '都道府県が未選択だと保存できないこと' do
         @payment.prefecture_id = 1
         @payment.valid?
         expect(@payment.errors.full_messages).to include('Prefecture must be other than 1')
       end
-  
+
       it '市区町村が空だと保存できないこと' do
         @payment.city = nil
         @payment.valid?
         expect(@payment.errors.full_messages).to include("City can't be blank")
       end
-  
+
       it '番地が空だと保存できないこと' do
         @payment.house_number = nil
         @payment.valid?
         expect(@payment.errors.full_messages).to include("House number can't be blank")
       end
-  
+
       it '電話番号が空だと保存できないこと' do
         @payment.phone_number = nil
         @payment.valid?
         expect(@payment.errors.full_messages).to include("Phone number can't be blank")
       end
-  
+
       it '電話番号が英数混合だと保存できないこと' do
-        @payment.phone_number = "090abcd-123"
+        @payment.phone_number = '090abcd-123'
         @payment.valid?
-        expect(@payment.errors.full_messages).to include("Phone number is not a number")
+        expect(@payment.errors.full_messages).to include('Phone number is not a number')
       end
-  
+
       it '電話番号が12桁以上だと保存できないこと' do
         @payment.phone_number = '090123456789'
         @payment.valid?

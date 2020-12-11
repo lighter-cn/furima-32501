cb16e123b3715e4593579ad2e38f3e3ce78e27b5
lighter <zeke.kircheis@gmail.com>
2020-12-11 10:54:50 +0900

    jsの関数を整理

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,15 +1,8 @@
 if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
   document.addEventListener('DOMContentLoaded', function(){
     const ImageList = document.getElementById('image-list')
-  
-    document.getElementById('item-image').addEventListener('change', function(e){
-      const imageContent = document.querySelector('img');
-      if (imageContent) {
-        imageContent.remove();
-      }
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
-  
+
+    const createImageHTML = (blob) => {
       const imageElement = document.createElement('div');
   
       const blobImage = document.createElement('img');
@@ -17,6 +10,18 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
   
       imageElement.appendChild(blobImage);
       ImageList.appendChild(imageElement);
+    }
+  
+    document.getElementById('item-image').addEventListener('change', function(e){
+      const imageContent = document.querySelector('img');
+      if (imageContent) {
+        imageContent.remove();
+      }
+      
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+  
+      createImageHTML(blob);
     });
   });
 }

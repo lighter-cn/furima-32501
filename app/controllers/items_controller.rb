3184f9c41c9e64b2ff7cc791802926f4d3250d02
lighter <zeke.kircheis@gmail.com>
2020-12-11 11:48:24 +0900

    不要なコメント削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,7 +45,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :delivery_date_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :delivery_date_id, :price,:image).merge(user_id: current_user.id)
   end
 
   def find_item
